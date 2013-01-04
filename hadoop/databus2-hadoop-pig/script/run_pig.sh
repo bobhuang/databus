f7ebb55f2a0e619c520ac6abf6ad0f9c7bedb748
Phanindra Ganti <pganti@linkedin.com>
2013-01-04 14:33:21 -0800

    removed dirs

--- hadoop/databus2-hadoop-pig/script/run_pig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-script_dir=`dirname $0`
-source ${script_dir}/setup.inc
-
-source ${script_dir}/hadoop-setup.inc
-
-PIG_JAVA_OPTS="-Dmapred.job.queue.name=marathon" pig $*
--- mysql-5.5/scripts/start-dbus-java.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-exit 0
-
-cd ~/databus2/trunk
-dist/databus2-relay-cmdline-pkg/bin/stop-espresso-relay.sh >/dev/null 2>&1 
-dist/databus2-relay-cmdline-pkg/bin/start-espresso-relay.sh >/dev/null  2>&1 
-sleep 5
