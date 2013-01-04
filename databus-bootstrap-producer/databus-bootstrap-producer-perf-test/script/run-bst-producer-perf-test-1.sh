35ec8c6f4452c002b0bb60e598c323fb0ede7a25
Phanindra Ganti <pganti@linkedin.com>
2013-01-04 14:32:20 -0800

    removed dirs

--- databus-bootstrap-producer/databus-bootstrap-producer-perf-test/script/run-bst-producer-perf-test-1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-script_dir=`dirname $0`
-source ${script_dir}/setup.inc
-
-${script_dir}/run-bst-producer-perf-test.sh -c ${conf_dir}/test_bootstrap_producer.conf -e ${conf_dir}/test_event_producer.conf $*
--- databus-bootstrap-producer/databus-bootstrap-producer-perf-test/script/run-bst-producer-perf-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir=`dirname $0`
-source $script_dir/setup.inc
-main_class=com.linkedin.databus2.bootstrap.producer.perf.ProducerSimulatorMain
-
-${script_dir}/database/databus2-bootstrap/bootstrap_db_reinit.sh 2>&1 > $log_dir/db_reinit.log
-
-java -cp ${cp} ${main_class} $*
